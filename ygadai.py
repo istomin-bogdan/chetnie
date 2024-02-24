3f10604b4c0753be6ca5137ece9986a8e3d4a45c
istomin-bogdan <istominbogdan40@gmail.com>
2024-02-24 20:16:47 +0700

    vbnm

--- ygadai.py
+++ ygadai.py
@@ -1,7 +1,11 @@
 from random import randint
-a = randint(1,10)
+a = randint(1,100)
 b = int(input('угадайте число от 1 до 10: '))
-if b == a:
-    print('правильно')
-else:
-    print('неправильно')
+while b != a:
+    print('неправильно, повторите попытку')
+    if a < b:
+        print('загаданое число меньше')
+    else:
+        print('загаданое число больше')
+    b = int(input('угадайте число от 1 до 10: '))
+print('верно, вы угадали')
